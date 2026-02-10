a418ce486f732f79819ff52afb89a810a183ebe6
SaharCHA <evanneva81@gmail.com>
2026-02-10 12:34:27 +0200

    Add post todo endpoint

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,6 +1,7 @@
 from typing import Annotated
+from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
-from fastapi import Depends, FastAPI, HTTPException, Path
+from fastapi import Depends, FastAPI, HTTPException, Path,status
 import models
 from database import engine,sessionmaker
 
@@ -18,6 +19,11 @@ def get_db(): # функция для получения сессии базы �
 
 db_dependency = Annotated[Session, Depends(get_db)] # создание зависимости для получения сессии базы данных, которая будет использоваться в обработчиках маршрутов для взаимодействия с базой данных
 
+class TodoRequest(BaseModel):
+     title: str = Field(min_length=3)
+     description: str = Field(min_length=3, max_length=100)
+     priority: int = Field(gt=0,lt=6)
+     completed: bool
 
 @app.get("/")
 async def read_all(db: db_dependency):
@@ -33,4 +39,12 @@ async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
         if todo is not None:
             return todo
         raise HTTPException(status_code=404, detail="Todo not found")
+
+
+@app.post("/todo", status_code=status.HTTP_201_CREATED) # обработчик для URL /todo, который создает новую задачу в базе данных. Он принимает данные задачи в формате JSON, проверяет их с помощью модели TodoRequest и сохраняет новую задачу в базе данных.
+async def create_todo(db: db_dependency, todo_request: TodoRequest):
+    """добавление новой задачи в базу данных."""
+    todo_request = models.Todo(**todo_request.model_dump())
+    db.add(todo_request)
+    db.commit()
     
