ce6d3a032e3b29c1ea7097fd788ff8af616b7f66
SaharCHA <evanneva81@gmail.com>
2026-02-17 16:49:09 +0200

    Add admini router

---
+++ TodoApp/routers/admin.py
@@ -0,0 +1,31 @@
+from typing import Annotated
+from pydantic import BaseModel, Field
+from sqlalchemy.orm import Session
+from fastapi import Depends, APIRouter, HTTPException, Path,status
+from database import db_dependency
+import models
+from .auth import get_current_user
+
+
+router = APIRouter(
+    prefix = "/admin",
+    tags=["admin"]
+) # создание приложения FastAPI
+
+user_dependency = Annotated[dict,Depends(get_current_user)]
+
+@router.get("/todo", status_code=status.HTTP_200_OK)
+async def read_all(user:user_dependency,db: db_dependency):
+    if user is None or user.get('user_role') != 'admin':
+        raise HTTPException(status_code=401,detail="Authentication failed")
+    return db.query(models.Todos).all()
+
+@router.delete("/todo/{todo_id}",status_code=status.HTTP_204_NO_CONTENT)
+async def delete_todo(user:user_dependency,db:db_dependency,todo_id:int=Path(gt=0)):
+    if user is None or user.get('user_role') != 'admin': 
+        raise HTTPException(status_code=401,detail='Authentication Failed')
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+    if todo_model is None:
+        raise HTTPException(status_code=401,detail="Todo not found")
+    db.query(models.Todos).filter(models.Todos.id == todo_id).delete()
+    db.commit()
